6adfc8e601baee1637567cc1c4cb32d366a9d288
Hannah Blair <hannahblair@hotmail.co.uk>
2020-05-06 21:57:59 +0100

    initial

---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+const merge = require("webpack-merge");
+const commonConfig = require("./webpack.config.common");
+
+module.exports = merge(commonConfig, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  rules: [
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"],
+    },
+  ],
+});
