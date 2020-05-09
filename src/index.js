b77f18902de418175dbc17f3399163cd9b2386c7
Hannah Blair <hannahblair@hotmail.co.uk>
2020-05-09 17:00:59 +0100

    Add prettier and  eslint config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class App extends React.Component {
+export class App extends React.Component {
   render() {
     return <div>Hello World</div>;
   }
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -13,9 +13,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /[\\/]node_modules[\\/]/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ["babel-loader", "eslint-loader"],
       },
     ],
   },
