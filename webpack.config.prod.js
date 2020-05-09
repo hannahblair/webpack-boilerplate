a18c5be62d8b1599bd8edec75a81937afc141c61
Hannah Blair <hannahblair@hotmail.co.uk>
2020-05-09 17:11:51 +0100

    Add image-loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,6 +13,19 @@ module.exports = merge(commonConfig, {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
